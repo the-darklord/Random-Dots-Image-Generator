5255c3cfaedca6d1414cd518019b6b5db82e8e14
the-darklord <saimihirnath@protonmail.com>
2024-07-04 12:56:47 +0530

    Update 1

--- dots.py
+++ dots.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 import random
+import os
+import csv
 
-def generate_image_with_dots(image_size=(500, 500), max_dots=100):
-    label_area_height = 150 + 10
-    total_height = image_size[0] + label_area_height
-    image = np.ones((total_height + 10, image_size[1] + 10, 3), dtype=np.uint8) * 255
+def generate_image_with_dots(image_size=(1080,1920), max_dots=100):
+    image = np.ones((image_size[0], image_size[1], 3), dtype=np.uint8) * 255
     
     colors = {
         'red': (0, 0, 255),
@@ -20,20 +20,20 @@ def generate_image_with_dots(image_size=(500, 500), max_dots=100):
     }
     
     occupied_positions = set()
-    dot_radius = 5
 
     def draw_dot(color, count_key):
         while True:
-            x = random.randint(label_area_height + dot_radius, total_height - dot_radius - 1)
+            dot_radius = random.randint(10,20)
+            x = random.randint(dot_radius, image_size[0] - dot_radius - 1)
             y = random.randint(dot_radius, image_size[1] - dot_radius - 1)
             if not any((x + dx, y + dy) in occupied_positions for dx in range(-dot_radius, dot_radius + 1) for dy in range(-dot_radius, dot_radius + 1)):
-                cv2.circle(image, (y, x), dot_radius, color, -1)
+                cv2.circle(image, (y, x), dot_radius - 1, color,-1)
                 for dx in range(-dot_radius, dot_radius + 1):
                     for dy in range(-dot_radius, dot_radius + 1):
                         occupied_positions.add((x + dx, y + dy))
                 counts[count_key] += 1
                 break
-    
+
     for _ in range(random.randint(1, max_dots)):
         draw_dot(colors['red'], 'red')
     
@@ -43,17 +43,18 @@ def generate_image_with_dots(image_size=(500, 500), max_dots=100):
     for _ in range(random.randint(1, max_dots)):
         draw_dot(colors['blue'], 'blue')
     
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(image, f'Red: {counts["red"]}', (10, 50), font, 1, colors['red'], 2, cv2.LINE_AA)
-    cv2.putText(image, f'Green: {counts["green"]}', (10, 100), font, 1, colors['green'], 2, cv2.LINE_AA)
-    cv2.putText(image, f'Blue: {counts["blue"]}', (10, 150), font, 1, colors['blue'], 2, cv2.LINE_AA)
-    
-    return image
+    return image, counts
 
 if __name__ == '__main__':
-    number_of_images = int(input("Enter Number of Clock Images to generate : "))
-    for i in range(1,number_of_images+1):
-        image_with_dots = generate_image_with_dots()
-        cv2.imwrite(f'images/dots_{i}.png', image_with_dots)
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    number_of_images = int(input("Enter Number of Clock Images to generate: "))
+    with open('labels.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['File Name', 'Red Count', 'Green Count', 'Blue Count'])
+        for i in range(1, number_of_images + 1):
+            image_with_dots, counts = generate_image_with_dots()
+            cv2.imwrite(f'images/dots_{i}.png', image_with_dots)
+            writer.writerow([f'dots_{i}.png', counts['red'], counts['green'], counts['blue']])
     print(f"Successfully generated {number_of_images} images")
     
